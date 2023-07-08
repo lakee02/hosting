e5590d185019de7bfe5a3c7f204d425cc6a893f6
lakee mangal <lakee.12002794@lpu.in>
2023-07-09 01:43:15 +0530

    new

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose=require('mongoose');
 const router = require('./routes/user-routes');
 const blogrouter = require('./routes/blog-routes');
 const app=express();
-
+const PORT=process.env.PORT;
 app.use(express.json());
 app.use('/api/user',router);
 app.use('/api/blog',blogrouter)
@@ -11,8 +11,8 @@ app.use('/api/blog',blogrouter)
 mongoose.connect(
     'mongodb+srv://admin:admin123@cluster0.xivwlwr.mongodb.net/Blog?retryWrites=true&w=majority'
 )
-.then(()=>app.listen(5000))
+.then(()=>app.listen(PORT))
 .then(()=>
-console.log("Connect to database and running port on 5000")
+console.log("Connect to database and running")
 )
 .catch((err)=>console.log(err));
